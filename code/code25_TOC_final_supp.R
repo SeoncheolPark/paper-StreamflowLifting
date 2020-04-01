bd8a64b30f9341b6fbb711a3f6ffd03245f54915
Seoncheol Park <pscstat@gmail.com>
2020-03-31 19:53:35 -0700

    Update code25_TOC_final_supp.R

--- code/code25_TOC_final_supp.R
+++ code/code25_TOC_final_supp.R
@@ -14,15 +14,15 @@ scatter_fill(TweedPredPoints$Longitude, TweedPredPoints$Latitude ,data_predicted
 points(example_network@obspoints@SSNPoints[[1]]@point.coords[,1], example_network@obspoints@SSNPoints[[1]]@point.coords[,2], pch=22)
 #quilt.plot(example_network@obspoints@SSNPoints[[1]]@point.coords[,1], example_network@obspoints@SSNPoints[[1]]@point.coords[,2], data, main="(a) Raw, TOC", cex=2, add=T, zlim=range(c(data, data_predicted, data_predicted0, data_predicted1)), xlab="", ylab="",smallplot=c(0.15,0.2,0.15,0.3), bigplot=c(0,0,1,1), xlim=range(TweedPredPoints$Longitude), ylim=range(TweedPredPoints$Latitude))
 
-scatter_fill(TweedPredPoints$Longitude, TweedPredPoints$Latitude ,data_predicted1_r[TweedPredPoints$StreamUnit], pch=16, cex=TweedPredPoints$Weights, xlab="", ylab="", zlim=zlims, main="(b) ODonnell - Raw", cex.main=1.5, smallplot=c(0.8,0.85,0.65,0.825), plot.legend=TRUE, axes=TRUE, y.axes=FALSE)
+scatter_fill(TweedPredPoints$Longitude, TweedPredPoints$Latitude ,-data_predicted1_r[TweedPredPoints$StreamUnit], pch=16, cex=TweedPredPoints$Weights, xlab="", ylab="", zlim=zlims, main="(b) ODonnell - Raw", cex.main=1.5, smallplot=c(0.8,0.85,0.65,0.825), plot.legend=TRUE, axes=TRUE, y.axes=FALSE)
 points(example_network@obspoints@SSNPoints[[1]]@point.coords[,1], example_network@obspoints@SSNPoints[[1]]@point.coords[,2], pch=22)
 #quilt.plot(example_network@obspoints@SSNPoints[[1]]@point.coords[,1], example_network@obspoints@SSNPoints[[1]]@point.coords[,2], denoising_ODonell, main="(b) ODonnell, TOC", cex=2, add=T, zlim=range(c(data, data_predicted, data_predicted0, data_predicted1)), xlab="", ylab="",smallplot=c(0.15,0.2,0.15,0.3))
 
-scatter_fill(TweedPredPoints$Longitude, TweedPredPoints$Latitude ,data_predicted_r[TweedPredPoints$StreamUnit], pch=16, cex=TweedPredPoints$Weights, xlab="", ylab="", zlim=zlims, main="(c) S-Lifting (M) - Raw", cex.main=1.5, smallplot=c(0.8,0.85,0.65,0.825))
+scatter_fill(TweedPredPoints$Longitude, TweedPredPoints$Latitude ,-data_predicted_r[TweedPredPoints$StreamUnit], pch=16, cex=TweedPredPoints$Weights, xlab="", ylab="", zlim=zlims, main="(c) S-Lifting (M) - Raw", cex.main=1.5, smallplot=c(0.8,0.85,0.65,0.825))
 points(example_network@obspoints@SSNPoints[[1]]@point.coords[,1], example_network@obspoints@SSNPoints[[1]]@point.coords[,2], pch=22)
 #quilt.plot(example_network@obspoints@SSNPoints[[1]]@point.coords[,1], example_network@obspoints@SSNPoints[[1]]@point.coords[,2], result_denoise, main="(c) S-Lifting (M), TOC", cex=2, add=T, zlim=range(c(data, data_predicted, data_predicted0, data_predicted1)), xlab="", ylab="",smallplot=c(0.15,0.2,0.15,0.3))
 
-scatter_fill(TweedPredPoints$Longitude, TweedPredPoints$Latitude ,data_predicted2_r[TweedPredPoints$StreamUnit], pch=16, cex=TweedPredPoints$Weights, xlab="", ylab="", zlim=zlims, main="(d) S-Lifting (N) - Raw", cex.main=1.5, smallplot=c(0.8,0.85,0.65,0.825), plot.legend=TRUE, axes=TRUE, y.axes=FALSE)
+scatter_fill(TweedPredPoints$Longitude, TweedPredPoints$Latitude ,-data_predicted2_r[TweedPredPoints$StreamUnit], pch=16, cex=TweedPredPoints$Weights, xlab="", ylab="", zlim=zlims, main="(d) S-Lifting (N) - Raw", cex.main=1.5, smallplot=c(0.8,0.85,0.65,0.825), plot.legend=TRUE, axes=TRUE, y.axes=FALSE)
 points(example_network@obspoints@SSNPoints[[1]]@point.coords[,1], example_network@obspoints@SSNPoints[[1]]@point.coords[,2], pch=22)
 #quilt.plot(example_network@obspoints@SSNPoints[[1]]@point.coords[,1], example_network@obspoints@SSNPoints[[1]]@point.coords[,2], result_nlt$aveghat, main="(d) S-Lifting (N), TOC", cex=2, add=T, zlim=range(c(data, data_predicted, data_predicted0, data_predicted1)), xlab="", ylab="",smallplot=c(0.15,0.2,0.15,0.3))
 
